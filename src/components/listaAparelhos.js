079f703410b1a01b0e15455792315f9ba21de7a9
k-alm <kauan.alm.almeida@gmail.com>
2023-09-07 22:32:36 -0300

    css aplicado e lista criada

---
+++ src/components/listaAparelhos.js
@@ -0,0 +1,17 @@
+export const listaAparelhos = [
+    {
+        id: 1,
+        nome: "Samsung",
+        preco: 5000
+    },
+    {
+        id: 2,
+        nome: "Tablet",
+        preco: 2000
+    },
+    {
+        id: 3,
+        nome: "iPhone",
+        preco: 3000000
+    }
+]
