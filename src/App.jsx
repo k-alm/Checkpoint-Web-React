354c107e9919ce89b04c260c2de129c5592a9cca
k-alm <kauan.alm.almeida@gmail.com>
2023-09-07 22:12:57 -0300

    App e main | react dom configurados

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
-import Home from './components/Home'
+import Cabecalho from './components/Cabecalho/Cabecalho'
+import Rodape from './components/Rodape/Rodape'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   return (
     <>
-      <div>
-        <Home/>
-      </div>
+      <Cabecalho/>
+      <Outlet/>
+      <Rodape/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
+//#region Imports
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter} from 'react-router-dom'
+import { RouterProvider } from 'react-router-dom' 
+import Aparelhos from './routes/Aparelhos/Aparelhos.jsx'
+import VisualizarAparelhos from './routes/VisualizarAparelhos/VisualizarAparelhos.jsx'
+import Home from './routes/Home/Home.jsx'
+import Erro404 from './routes/Error/Erro404.jsx'
+//#endregion
+
+const router = createBrowserRouter([
+  {
+    path: '/', 
+    element: <App/>,
+    errorElement: <Erro404/>,
+    children:[
+      {path: '/', element: <Home/>},
+      {path: '/aparelhos', element: <Aparelhos/>},
+      {path: '/visualizar/aparelhos/:id', element: <VisualizarAparelhos/>},
+    ]
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
